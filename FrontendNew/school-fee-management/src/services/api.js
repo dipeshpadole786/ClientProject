47955159bfd6a7ac89b86d424fabc62189de8511
Dipesh Padole <sudhirpadole00@gmail.com>
2026-07-10 06:49:36 +0530

    Update api.js

--- FrontendNew/school-fee-management/src/services/api.js
+++ FrontendNew/school-fee-management/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = 'https://school-iemr.onrender.com/api'
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('sfms_auth_token')
