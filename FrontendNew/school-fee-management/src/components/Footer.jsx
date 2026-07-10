befb0df7b54afa3557f54a32c5a3634681c7116b
Dipesh Padole <sudhirpadole00@gmail.com>
2026-07-10 07:16:14 +0530

    Update Footer.jsx

--- FrontendNew/school-fee-management/src/components/Footer.jsx
+++ FrontendNew/school-fee-management/src/components/Footer.jsx
@@ -1,15 +1,16 @@
 function Footer() {
   return (
     <footer className="w-full py-4 text-center text-sm text-gray-500 border-t border-gray-200 bg-white">
-      Made by Dipesh <span className="mx-1">&lt;&gt;</span>{' '}
+      Made by{' '}
       <a
         href="https://dipesh-padole-1.onrender.com/"
         target="_blank"
         rel="noreferrer"
         className="font-semibold text-gray-700 hover:text-gray-900 hover:underline"
       >
-        dipesh-padole-1.onrender.com
-      </a>
+        Dipesh Padole
+      </a>{' '}
+      {'\u{1F9D1}\u200D\u{1F4BB}'}
     </footer>
   )
 }
